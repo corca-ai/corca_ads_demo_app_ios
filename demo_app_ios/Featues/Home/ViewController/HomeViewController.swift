1af6f483ddb8f2b2581977370d0266ff91fac8d1
MANSA10004 <mansa@corca.ai>
2025-01-13 16:37:23 +0900

    feat: 주석 수정

--- demo_app_ios/Featues/Home/ViewController/HomeViewController.swift
+++ demo_app_ios/Featues/Home/ViewController/HomeViewController.swift
@@ -11,14 +11,8 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
     private var presenter: HomePresenter?
-    
-    /// - key: IndexPath(어느 셀인지)
-    /// - value: DispatchWorkItem (1초 후 onImpression 호출을 담당)
     private var visibleCellsWorkItems: [IndexPath: DispatchWorkItem] = [:]
-    
-    /// 1초 이상 노출 시 Impression을 찍기 위해 1초 스케줄
     private let impressionThreshold: TimeInterval = 1.0
-    
     private let storeID = "7bbb703e-a30b-4a4a-91b4-c0a7d2303415"
     private let userAgent: String = "\(UIDevice.current.systemName)\(UIDevice.current.systemVersion)"
     
@@ -62,15 +56,12 @@ class HomeViewController: UIViewController {
         cleanupWorkItems()
     }
     
-    // MARK: - Private Methods
-    
-    /// nib 등록
     private func registerXib() {
         let nibName = UINib(nibName: PlacementCell.cellName, bundle: nil)
         collectionView.register(nibName, forCellWithReuseIdentifier: PlacementCell.cellReuseIdentifier)
     }
     
-    /// 셀이 50% 이상 노출되었는지, 1초 이상 유지되었는지 확인
+    // 기준1.  셀이 50% 이상 노출되었는지, 1초 이상 유지되었는지 확인
     private func checkVisibleCells() {
         for cell in collectionView.visibleCells {
             if let indexPath = collectionView.indexPath(for: cell),
@@ -87,36 +78,30 @@ class HomeViewController: UIViewController {
                 
                 guard let cellFrame = cellFrame else { continue }
                 
-                // 화면에 실제로 보이는 영역과 셀의 영역 교집합 구함
                 let intersection = visibleRect.intersection(cellFrame)
                 let visibleArea = intersection.width * intersection.height
                 let cellArea = cellFrame.width * cellFrame.height
                 
-                // 50% 이상 노출 여부
                 if visibleArea / cellArea >= 0.5 {
-                    // 만약 50% 이상 노출되면 '1초 뒤에' onImpression 호출을 스케줄링
                     scheduleImpression(for: indexPath,
                                        advertisementProduct: adProducts[indexPath.item])
                 } else {
-                    // 50% 미만으로 내려가면 기존에 스케줄링된 onImpression 취소
                     cancelScheduledImpression(for: indexPath)
                 }
             }
         }
     }
     
-    /// 1초 뒤 onImpression 호출을 스케줄링
     private func scheduleImpression(for indexPath: IndexPath,
                                     advertisementProduct: AdvertisementItem) {
-        // 이미 스케줄링 중이라면 중복으로 스케줄 X
+        // 기준3. 이미 스케줄링 중이라면 중복으로 스케줄 X
         if visibleCellsWorkItems[indexPath] != nil {
             return
         }
         
         let workItem = DispatchWorkItem { [weak self] in
             guard let self = self else { return }
-            
-            // onImpression 호출
+        
             self.onImpression(
                 appVersion: self.appVersion,
                 storeId: self.storeID,
@@ -124,18 +109,17 @@ class HomeViewController: UIViewController {
                 requestId: advertisementProduct.requestId,
                 adsetId: advertisementProduct.adsetId
             )
-            // 스케줄링 목록에서 제거
+        
             self.visibleCellsWorkItems[indexPath] = nil
         }
         
         visibleCellsWorkItems[indexPath] = workItem
         
-        // 1초 후 실행 (impressionThreshold 초)
+        // 기준2. 1초 후 실행
         DispatchQueue.main.asyncAfter(deadline: .now() + impressionThreshold,
                                       execute: workItem)
     }
     
-    /// 스케줄링된 1초 뒤 onImpression 호출을 취소
     private func cancelScheduledImpression(for indexPath: IndexPath) {
         if let workItem = visibleCellsWorkItems[indexPath] {
             workItem.cancel()
@@ -143,7 +127,6 @@ class HomeViewController: UIViewController {
         }
     }
     
-    /// 화면을 떠나면 WorkItem 전체 정리
     private func cleanupWorkItems() {
         for workItem in visibleCellsWorkItems.values {
             workItem.cancel()
@@ -151,7 +134,6 @@ class HomeViewController: UIViewController {
         visibleCellsWorkItems.removeAll()
     }
     
-    /// 무한 스크롤 시 데이터 로드
     private func loadMoreData() async {
         guard !isLoadingMoreData else { return }
         isLoadingMoreData = true
@@ -183,8 +165,6 @@ extension HomeViewController: UICollectionViewDataSource {
             cell.configure(adProducts[indexPath.item])
         }
         
-        // 셀이 로드될 때마다 한 번씩 체크 → 매번 cellForItemAt에서 call
-        // (추가적으로 scrollViewDidScroll에서 호출해도 됨)
         checkVisibleCells()
         
         return cell
@@ -214,16 +194,13 @@ extension HomeViewController: UICollectionViewDelegate {
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        // 스크롤할 때마다 50% 노출 체크
         checkVisibleCells()
         
-        // 무한 스크롤 체크
         if let presenter = presenter {
             let contentHeight = scrollView.contentSize.height
             let offsetY = scrollView.contentOffset.y
             let scrollViewHeight = scrollView.frame.size.height
             
-            // 임의로 scrollViewHeight * 2 지점에서 추가 로드
             if offsetY > contentHeight - scrollViewHeight * 2 {
                 let visibleCells = collectionView.visibleCells
                 let indexPaths = visibleCells.compactMap { collectionView.indexPath(for: $0) }
